6f99b600b519956ac62c76b596e9024fbc9c0f39
Alvaro tarquiola <alvarotarquiola@gmail.com>
2013-08-17 15:51:41 -0400

    ignore

---
+++ tools/smarty/compile/%%3A^3A9^3A9FC827%%index.tpl.php
@@ -0,0 +1,3 @@
+<?php /* Smarty version 2.6.20, created on 2013-08-17 18:36:14
+         compiled from D:%5Cxampp%5Chtdocs%5Ctienda-cliente/themes/Rojo/index.tpl */ ?>
+<?php echo $this->_tpl_vars['HOOK_HOME']; ?>
---
+++ tools/smarty/compile/%%ED^EDE^EDE869BC%%header.tpl.php
@@ -0,0 +1,8 @@
+<?php /* Smarty version 2.6.20, created on 2013-08-17 18:36:11
+         compiled from D:%5Cxampp%5Chtdocs%5Ctienda-cliente%5Cmodules%5Cblocksearch/header.tpl */ ?>
+<!-- Block search module HEADER -->
+<link rel="stylesheet" type="text/css" href="<?php echo $this->_tpl_vars['content_dir']; ?>
+css/jquery.autocomplete.css" />
+<script type="text/javascript" src="<?php echo $this->_tpl_vars['content_dir']; ?>
+js/jquery/jquery.autocomplete.js"></script>
+<!-- Block search module HEADER -->
